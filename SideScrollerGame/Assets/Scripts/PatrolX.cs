291620ee8b7da2b46469ab64a0dd5d2b1cafb40b
Daniel <mydjp0912@yahoo.com>
2019-04-06 15:18:22 -0600

    working on cthulhu

---
+++ SideScrollerGame/Assets/Scripts/PatrolX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolX : MonoBehaviour
+{
+
+    public float speed = 5;
+    
+    
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(1*speed,0,0);
+    }
+    
+    private void OnTriggerEnter(Collider obj)
+    {
+        if(obj.gameObject.CompareTag("Patrol"))
+        {
+            speed = speed * -1;
+        }
+    }
+
+    
+    
+}
---
+++ SideScrollerGame/Assets/Scripts/PatrolY.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class PatrolY : MonoBehaviour
+{
+
+    public float speed = 5;
+    
+    
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0,1*speed,0);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (tag == "Patrol")
+        {
+            speed = speed * -1;
+        }
+    }
+}
