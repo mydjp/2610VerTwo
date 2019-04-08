a67f67b5bb5f35db6ae0a1fdac43e6425c975bcc
Daniel <mydjp0912@yahoo.com>
2019-04-07 20:32:41 -0600

    working on CC for SHips

---
+++ SideScrollerGame/Assets/Scripts/Constant CC move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantCCmove : MonoBehaviour
+{
+    public float speed = 5;
+    
+    private CharacterController ShipCC;
+    
+   
+    
+    void Start()
+    {
+        ShipCC = GetComponent<CharacterController>();
+    }
+    
+    
+    
+    void Update()
+    {
+        GetComponent<CharacterController>().velocity = new Vector3(1*speed,0,0);
+    }
+}
