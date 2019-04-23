fbba8d413ff66f280b1dcb0884bf0b9e655f5a38
daniel <mydjp0912@yahoo.com>
2019-04-23 15:41:58 -0600

    got tiny ship to work need to parent

---
+++ SideScrollerGame/Assets/Scripts/TriggerChild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChild : MonoBehaviour
+{
+
+    public GameObject player;
+    
+    
+
+   
+    private void OnCollisionEnter(Collision obj)
+        {
+            if(obj.gameObject.CompareTag("Player"))
+            {
+                player.transform.SetParent(transform);
+            }
+        }
+    
+}
