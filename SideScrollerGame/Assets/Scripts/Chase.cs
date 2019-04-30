37bbcc58c84c54193c86837ce1bb95f515e117dc
daniel <mydjp0912@yahoo.com>
2019-04-30 08:39:26 -0600

    little tweeks

---
+++ SideScrollerGame/Assets/Scripts/Chase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public GameObject other;
+    public float distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(this.transform.position, other.transform.position);
+
+        if (distance> 100)
+        {
+            Debug.Log(distance);
+
+            distance = 100;
+            transform.position = (transform.position - other.transform.position).normalized * distance +
+                                 other.transform.position;
+            
+        }
+            
+    }
+}
